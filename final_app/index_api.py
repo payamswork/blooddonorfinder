34cff9d595aa4b8e7a371aeecfcc28bf735093ee
payamswork <payambilal874@gmail.com>
2026-06-28 16:25:46 +0530

    Delete final_app/index_api.py

--- final_app/index_api.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Make sure the root project directory is on the path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
-
-# Vercel expects a variable named `app`
